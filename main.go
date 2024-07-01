3971edc2cbb6f2eb87bd9f2b0041707c963ebf72
masgal <dimasrumekso28@gmail.com>
2024-07-01 20:40:05 +0700

    tambahan pesan

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hallo ini fitur login")
-	fmt.Println("ini progress pengerjaan dari fitur register")
-	fmt.Println("ini progress pengerjaan dari fitur register 2")
+	fmt.Println("hallo ini pesan penyemangat buat kawan-kawan DE3")
+	fmt.Println("Semangat ges yak, setiap orang yang rajin...")
+	fmt.Println("pasti akan mendapatkan bagiannya")
 }
