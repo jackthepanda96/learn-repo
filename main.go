bdea8fa4b00803f1029c9340293ea72966689e5c
Jerry Young <jerryyoung2911@gmail.com>
2024-07-01 20:37:24 +0700

    add visualisasi

--- main.go
+++ main.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("hallo ini fitur login")
-	fmt.Println("ini progress pengerjaan dari fitur register")
 	fmt.Println("ini progress pengerjaan dari fitur register 2")
+	fmt.Println("ini demo untuk DE, mari belajar bersama")
 }
